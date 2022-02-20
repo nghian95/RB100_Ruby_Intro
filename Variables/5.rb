d81f25d5a8fa32cb8d9837138d1bfd96bfa4e59b
Nghia Nguyen <nghian95@gmail.com>
2022-02-20 15:05:27 -0600

    Variables Exercises

---
+++ Variables/5.rb
@@ -0,0 +1,14 @@
+# What is the output of each?
+
+x = 0
+3.times do
+  x += 1
+end
+puts x
+
+y = 0
+3.times do
+  y += 1
+  a = y
+end
+puts a 
---
+++ Variables/name.rb
@@ -0,0 +1,6 @@
+puts "Please type your first name:"
+first_name = gets.chomp
+puts "Last name:"
+last_name = gets.chomp
+puts "Greetings, #{first_name} #{last_name}."
+10.times {puts "#{first_name} #{last_name}"}
