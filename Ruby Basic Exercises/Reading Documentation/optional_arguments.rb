8d748e70f035e22747e4eb6f11d65c0b60e0a571
Nghia Nguyen <nghian95@gmail.com>
2022-03-07 18:47:29 -0600

    Ruby Basic Exercises folder

---
+++ Ruby Basic Exercises/Reading Documentation/optional_arguments.rb
@@ -0,0 +1,13 @@
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect
+puts s.split(',').inspect
+puts s.split(',',2).inspect
+
+# puts s.split.inspect
+#["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
+
+# puts s.split(',').inspect
+#["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+
+# puts s.split(',', 2).inspect
+#["abc def ghi", "jkl mno pqr,stu vwx yz"]
